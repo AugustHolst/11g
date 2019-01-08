8d741f77942fcd496e8f17cc8c30527fd11765e1
August Heining Holst <Augustheiningholst@gmail.com>
2019-01-08 19:46:54 +0100

    opgave 11g.0 løst

--- chess.fs
+++ chess.fs
@@ -19,7 +19,7 @@ type chessPiece(color : Color) =
   /// [[(1,0); (2,0);...]; [(-1,0); (-2,0)]...]. Runs must be
   /// ordered such that the first in a list is closest to the piece
   /// at hand.      
-  abstract member candiateRelativeMoves : Position list list
+  abstract member candidateRelativeMoves : Position list list
   /// Available moves and neighbours ([(1,0); (2,0);...], [p1; p2])
   abstract member availableMoves : Board -> (Position list * chessPiece list)
   default this.availableMoves (board : Board) : (Position list * chessPiece list) =
@@ -98,7 +98,7 @@ and Board () =
       | Some p ->
         let convertNWrap = 
           (relativeToAbsolute p) >> this.getVacantNOccupied
-        let vacantPieceLists = List.map convertNWrap piece.candiateRelativeMoves
+        let vacantPieceLists = List.map convertNWrap piece.candidateRelativeMoves
         // Extract and merge lists of vacant squares
         let vacant = List.collect fst vacantPieceLists
         // Extract and merge lists of first obstruction pieces and filter out own pieces
--- pieces.fs
+++ pieces.fs
@@ -5,42 +5,39 @@ type king(col : Color) =
   inherit chessPiece(col)
   override this.nameOfType = "king"
   // king has runs of 1 in 8 directions: (N, NE, E, SE, S, SW, W, NW)
-  override this.candiateRelativeMoves =
+  override this.candidateRelativeMoves =
       [[(-1,0)];[(-1,1)];[(0,1)];[(1,1)];
       [(1,0)];[(1,-1)];[(0,-1)];[(-1,-1)]]
 
+/// A king's available moves is limited, since it cannot move into a losing position.
+/// The function works by calculating opponents possible moves and filtering the list so only valid moves are left.  
   override this.availableMoves (board : Board) : (Position list * chessPiece list) =
+    let kingRank = fst this.position.Value
+    let kingFile = snd this.position.Value
+    let tmpKing = this :> chessPiece
+    board.[kingRank,kingFile] <- None //Remove king from board to calculate proper rook moves.
     let mutable invalidSquares = []
-    let neighbouringPieces = snd (board.getVacantNNeighbours this)
+    let neighbouringPieces : chessPiece list = snd (board.getVacantNNeighbours this)
     if this.color = White then 
       for r = 0 to 7 do
         for f = 0 to 7 do
-          if board._array.[r,f].isSome && board._array.[r,f].Value.color = Black then
-            let attackingSquares = fst (board.getVacantNNeighbours board._array.[r.f].Value)
+          let square = board.[r,f]
+          if Option.isSome square && square.Value.color = Black then
+            let attackingSquares = fst (board.getVacantNNeighbours (square.Value))
             invalidSquares <- invalidSquares @ attackingSquares
     else
       for r = 0 to 7 do
         for f = 0 to 7 do
-          if board._array.[r,f].isSome && board._array.[r,f].Value.color = White then
-            let attackingSquares = fst (board.getVacantNNeighbours board._array.[r.f].Value)
+          let square = board.[r,f]
+          if Option.isSome square && square.Value.color = White then
+            let attackingSquares = fst (board.getVacantNNeighbours square.Value)
             invalidSquares <- invalidSquares @ attackingSquares
     let naiveValidSquares = fst (board.getVacantNNeighbours this)
-    let validSquares = List.filter
+    let isSquareInvalid pos = List.contains pos invalidSquares
+    let validSquares : Position list = List.filter (fun pos -> not (isSquareInvalid pos)) naiveValidSquares
+    board.[kingRank,kingFile] <- Some tmpKing //Put king back on the board
+    (validSquares, neighbouringPieces)
     
-    (*let mutable validSquares : (Position list) = []
-    let r = fst this.position.Value
-    let f = snd this.position.Value
-    let possiblePositions = [|Some (r-1, f-1); Some (r, f-1); Some (r+1, f-1); Some (r-1, f); Some (r+1, f); Some (r-1, f+1); Some (r, f+1); Some (r+1, f+1)|]
-    let possibleKings = List.init 8 (fun x -> king (this.color))
-    for i = 0 to 7 do
-      possibleKings.[i].position <- possiblePositions.[i]
-      if List.exists (fun cp -> cp.toString = 'K') (snd (board.getVacantNNeighbours possibleKings.[i])) then 
-        ()
-      else 
-        validSquares <- possiblePositions.[i] :: validSquares
-    let neighbouringPieces = snd (board.getVacantNNeighbours this)
-    List.zip validSquares neighbouringPieces*)
-
 /// A rook is a chessPiece which moves horisontally and vertically
 type rook(col : Color) =
   inherit chessPiece(col)
@@ -60,6 +57,6 @@ type rook(col : Color) =
   // For each function in indToRel, we calculate List.map f [1..7].
   // swap converts (List.map fct indices) to (List.map indices fct).
   let swap f a b = f b a
-  override this.candiateRelativeMoves =
+  override this.candidateRelativeMoves =
     List.map (swap List.map [1..7]) indToRel (*//§\label{chessPieceSwapApp}§*)
   override this.nameOfType = "rook"
